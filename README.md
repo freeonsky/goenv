d8e020443c07516ea952022851676dc9e960b511
freeonsky <freeonsky@qq.com>
2018-02-15 17:50:30 +0800

    add goenv get

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 
 # 安装
 go get github.com/freeonsky/goenv
+
+安装好之后，先运行 goenv 程序，当其实是否将 goenv 拷贝到 $GOROOT/bin 时，请输入y，这样就能在cmd/powershell下执行 goenv
+
+# 使用
+goenv get url
+例如：
+goenv get get github.com/nsf/gocode
+会将 gocode 安装到当前目录下，并设置 GOPATH 为当前目录
--- main.go
+++ main.go
@@ -1,38 +1,103 @@
 package main
 
 import (
+	"path"
 	"runtime"
 	"os/exec"
 	"os/user"
 	"os"
+	"io"
+	"path/filepath"
 	"log"
 	"fmt"
+	"strings"
+	"errors"
+
 )
+
+func getCurrentExecPath() string {
+	fpath, _ := exec.LookPath(os.Args[0])
+	log.Print("filepath",fpath)
+	dir := filepath.Dir(fpath)
+	log.Print("dir",dir)
+	return dir
+}
+
+func CopyFile(src string, dest string) (int64, error) {
+	srcFile,err := os.Open(src)
+	if err != nil {
+		fmt.Println(err.Error())
+		return -1, errors.New("open src" + src + " fail")
+	}
+	defer srcFile.Close()
+
+	destFile,err := os.Create(dest)
+	if err != nil {
+		fmt.Println(err.Error())
+		return -1, errors.New("create file " + dest + " fail")		
+	}
+	defer destFile.Close()
+	return io.Copy(destFile, srcFile)
+}
+
+func setGOROOTEnv(gopath string){
+	cmd := exec.Command("setx", "GOPATH", gopath)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stdout.Close()
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("set GOPATH=", gopath)
+	}
+}
 
 func main() {
 	goos := runtime.GOOS
 	currentUser,_ := user.Current()
+	goroot := os.Getenv("GOROOT")
+	thisFile := os.Args[0]
+	_, thisFileName := filepath.Split(thisFile)
 	log.Printf("os:%s\n", goos)
 	log.Printf("home:%s\n", currentUser.HomeDir)
-	
+	log.Printf("GOROOT:%s\n", goroot)
 	if goos == "windows" {
-		gopath := currentUser.HomeDir
-		if len(os.Args) == 2 {
-			gopath = os.Args[1]
+		// no parameter
+		if len(os.Args) == 1 {
+			
+			
+			fmt.Printf("will you copy %s to %s:", thisFile, goroot)
+			var s string
+			fmt.Scanf("%s", &s)
+			if !strings.EqualFold("y", s) {
+				return
+			}
+			CopyFile(thisFile, path.Join(goroot,"bin", thisFileName))
+			return 
 		}
-		log.Println("gopath:", gopath)
+		if len(os.Args) == 3 {
+			if os.Args[1] == "get" {
+				pwd,_ := os.Getwd()
+				if goroot != pwd {
+					setGOROOTEnv(pwd)
+				}
 
-		cmd := exec.Command("setx", "GOPATH", gopath)
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Fatal(err)
+				os.Setenv("GOPATH",pwd)
+				cmd := exec.Command("go", "get", os.Args[2])
+				stdout, err := cmd.StdoutPipe()
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer stdout.Close()
+				if err := cmd.Start(); err != nil {
+					log.Fatal(err)
+				}
+			}
 		}
-		defer stdout.Close()
-		if err := cmd.Start(); err != nil {
-			log.Fatal(err)
-		} else {
-			fmt.Println("set GOPATH=", gopath)
-		}
+		
+
 
 	}
 	
